5d31e44f62b006f4781ecd8e4e48d068e4ad865c
LolloPassa <lollopassa@icloud.com>
2026-08-12 17:43:58 +0200

    Aggiornamento 1.0.8.3 Aggiunta Gabi e Riccardo

---
+++ js/Characters/gabiRaimonGO.js
@@ -0,0 +1,36 @@
+import { characterRegistry } from './registry.js';
+
+const baseInfo = characterRegistry.find(char => char.id === "gabiRaimonGO");
+
+export const charData = {
+    ...baseInfo,
+    name: "Gabi Garcia",
+    romanizedName: "Kirino Ranmaru",
+    japaneseName: "霧野 蘭丸",
+
+    tags: [
+        "img/TagTitle/Icon_Tag_Team_RevolutionRaimon.png",
+        "img/TagTitle/Icon_Tag_Ability_Shootblocker.png",
+        "img/TagTitle/Icon_Tag_Title_InaEleGo1.png"
+    ],
+
+
+    stats: {
+        "TP": { lv1: 100, lv320: 100, icon: "img/Status/Icon_Status_TP.png" },
+        "Tiro": { lv1: 25, lv320: 1960, icon: "img/Status/Icon_Status_Kick.png" },
+        "Tecnica": { lv1: 49, lv320: 4558, icon: "img/Status/Icon_Status_Technic.png" },
+        "Blocco": { lv1:69, lv320: 6586, icon: "img/Status/Icon_Status_Block.png" },
+        "Parata": { lv1:38, lv320: 3392, icon: "img/Status/Icon_Status_Catch.png" },
+        "Velocità": { lv1: 4, lv320: 4, icon: "img/Status/Icon_Status_Speed.png" }
+    },
+
+    zones: [
+        { code: 9, rank: 'S' },
+        { code: 7, rank: 'A' },
+        { code: 8, rank: 'B' }
+    ],
+
+    myTechniques: ["ザ・ミスト", "ディープミスト"],
+    myBasicPassivesIds: ["100000301", "104003001", "104003003"],
+    myRarityPassivesIds: ["100000501", "104003004"]
+};
---
+++ js/Characters/riccardoDiRigoRaimonGO.js
@@ -0,0 +1,35 @@
+import { characterRegistry } from './registry.js';
+
+const baseInfo = characterRegistry.find(char => char.id === "riccardoDiRigoRaimonGO");
+
+export const charData = {
+    ...baseInfo,
+    name: "Riccardo Di Rigo",
+    romanizedName: "Shindou Takuto",
+    japaneseName: "神童 拓人",
+
+    tags: [
+        "img/TagTitle/Icon_Tag_Team_RevolutionRaimon.png",
+        "img/TagTitle/Icon_Tag_Ability_Playmaker.png",
+        "img/TagTitle/Icon_Tag_Title_InaEleGo1.png"
+    ],
+
+    stats: {
+        "TP": { lv1: 100, lv340: 100, icon: "img/Status/Icon_Status_TP.png" },
+        "Tiro": { lv1: 57, lv340: 1960, icon: "img/Status/Icon_Status_Kick.png" },
+        "Tecnica": { lv1: 64, lv340: 4558, icon: "img/Status/Icon_Status_Technic.png" },
+        "Blocco": { lv1: 33, lv340: 2846, icon: "img/Status/Icon_Status_Block.png" },
+        "Parata": { lv1: 24, lv340: 1898, icon: "img/Status/Icon_Status_Catch.png" },
+        "Velocità": { lv1: 6, lv340: 6, icon: "img/Status/Icon_Status_Speed.png" }
+    },
+
+    zones: [
+        { code: 5, rank: 'S' },
+        { code: 7, rank: 'A' },
+        { code: 2, rank: 'B' }
+    ],
+
+    myTechniques: ["フォルテシモ", "プレストターン"],
+    myBasicPassivesIds: ["100000201", "101100001", "101100003"],
+    myRarityPassivesIds: ["100000501", "101100004"]
+};
