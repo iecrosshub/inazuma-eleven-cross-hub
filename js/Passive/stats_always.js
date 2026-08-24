5809af44f7545b6bee91cc81999fc169f220d241
LolloPassa <lollopassa@icloud.com>
2026-08-25 00:48:58 +0200

    Aggiornamento 1.0.8.10 Aggiunte Manager

--- js/Passive/stats_always.js
+++ js/Passive/stats_always.js
@@ -2306,27 +2306,4 @@ export const alwaysPassives = [
         ]
     },
 
-    {
-        title: "Tiro + (キック＋)",
-        id: "100000101",
-        template: "All'inizio della partita, aumenta il proprio Tiro (Kick) di {VAL}.",
-        levels: [
-            { val: 67, req: "Si sblocca con personaggio Lv. 11" },
-            { val: 134, req: "Si sblocca con personaggio Lv. 51" },
-            { val: 201, req: "Si sblocca con personaggio Lv. 81" },
-            { val: 268, req: "Si sblocca con personaggio Lv. 111" },
-            { val: 403, req: "Si sblocca con personaggio Lv. 141" },
-            { val: 537, req: "Si sblocca con personaggio Lv. 171" },
-            { val: 672, req: "Si sblocca con personaggio Lv. 201" },
-            { val: 806, req: "Si sblocca con personaggio Lv. 231" },
-            { val: 1075, req: "Si sblocca con personaggio Lv. 261" },
-            { val: 1344, req: "Si sblocca con personaggio Lv. 291" },
-            { val: 1545, req: "Si sblocca con personaggio Lv. 321" }
-        ],
-        category: "Always",
-        conditions: null,
-        effects: [
-            { targetScope: "self", targetRoles: [], targetElements: [], type: "stat", statName: "Tiro", valueRef: "val" }
-        ]
-    }
 ];
